8b5544b9918fabe8f8dbd4d58aee5b302adef8eb
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2025-08-16 11:14:56 -0300

    adicionando algoritmos a implementação das árvores

--- Arvores/Arvore.h
+++ Arvores/Arvore.h
@@ -1,6 +1,8 @@
 #ifndef ARVORE_H
 #define ARVORE_H
+
 #include <vector>
+#include <print>
 
 template<typename T>
 class Node {
@@ -48,15 +50,7 @@ private:
 template<typename T>
 class Arvore {
 public:
-
-    // posição do nó na árvore
-    [[nodiscard]] auto get_depth() const -> int {
-        // if
-    }
-
-    [[nodiscard]] auto size() const -> std::size_t {
-        return 0;
-    }
+    [[nodiscard]] auto size() const -> std::size_t { return 0; }
 
     [[nodiscard]] auto is_empty() const -> bool { return true; }
 
@@ -64,8 +58,38 @@ public:
 
     [[nodiscard]] auto get_nodes() const -> std::vector<Node<T> *>;
 
+    // [Algoritmos]
+
+    // Retorna a altura de um nó em uma árvore
+    [[nodiscard]] static auto get_height(const Node<T> &node) -> int {
+        if (node.is_leaf())
+            return 0;
+        int height{};
+        for (auto n: node.m_children)
+            height = max(height, get_height(n));
+        return 1 + height;
+    }
+
+    // Retorna a profundidade de um nó em uma árvore
+    [[nodiscard]] static auto get_depth(const Node<T> &node) -> int {
+        if (node.is_root())
+            return 0;
+        return 1 + get_depth(node.get_parent());
+    }
+
+    // [Algoritmos] - Acesso aos nós
+
+    // Mostra o nó console e depois acessa seus filhos
+    static auto preorder_print(const Node<T> &node) -> void {
+        std::print(*node);
+        for (auto child : node.get_children()) {
+            std::print(" ");
+            preorder_print(child);
+        }
+    }
+
 private:
     Node<T> *m_root{nullptr};
 };
-//
+
 #endif //ARVORE_H
