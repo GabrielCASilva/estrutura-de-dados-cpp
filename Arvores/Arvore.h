0e5b48682bfd05bd8f93aaa0a625ce0c53fc1fbb
GabrielCASilva <gabriel.foxcarvalho@gmail.com>
2025-08-18 21:37:55 -0300

    proceguindo na implementação de uma arvore

--- Arvores/Arvore.h
+++ Arvores/Arvore.h
@@ -11,51 +11,95 @@ public:
 
     ~Node() = default;
 
-    Node(const Node &rhs) = delete;
+    explicit Node(T value) {
+        m_value = value;
+    }
 
-    Node &operator=(const Node &rhs) = delete;
+    Node(const Node<T> &rhs){
+        if (this == &rhs)
+            return;
+        m_value = rhs.m_value;
+        m_parent = new Node<T>();
+        m_parent->m_value = rhs.m_parent->m_value;
+        for (auto child : rhs.m_children){
+            m_children.push_back(child);
+        }
+    }
 
-    Node(Node &&rhs) = delete;
+    Node &operator=(const Node<T> &rhs){
+        if (this == &rhs)
+            return *this;
+        m_value = rhs.m_value;
+        m_parent = new Node<T>();
+        m_parent->m_value = rhs.m_parent->m_value;
+        for (auto child : rhs.m_children){
+            m_children.push_back(child);
+        }
+        return *this;
+    };
 
-    Node &operator=(Node &&rhs) = delete;
+    Node(Node &&lhs) = delete;
+
+    Node<T> &operator=(Node<T> &&lhs) = delete;
 
     T &operator*() { return m_value; }
+    const T &operator*() const { return m_value; }
 
-    [[nodiscard]] auto get_parent() const -> Node {
-        return m_parent;
+    T &operator!() { return m_value != nullptr; }
+    const T &operator!() const { return m_value != nullptr; }
+
+    auto add_child(Node<T> &node) -> void {
+        node.m_parent = this;
+        m_children.push_back(&node);
     }
 
-    [[nodiscard]] auto get_children() const -> std::vector<Node *> & {
-        return m_children;
-    }
+    [[nodiscard]] auto get_parent() const -> Node<T> * { return m_parent; }
 
-    [[nodiscard]] auto is_leaf() const -> bool {
-        return get_children().empty();
-    }
+    [[nodiscard]] auto get_children() const -> const std::vector<Node<T> *> & { return m_children; }
+    [[nodiscard]] auto get_children() -> std::vector<Node<T> *> & { return m_children; }
 
-    [[nodiscard]] auto is_root() const -> bool {
-        return not get_parent();
-    }
+    [[nodiscard]] auto is_leaf() const -> bool { return get_children().empty(); }
 
-    [[nodiscard]] auto is_internal() const -> bool {
-        return not is_leaf();
-    }
+    [[nodiscard]] auto is_root() const -> bool { return !get_parent(); }
+
+    [[nodiscard]] auto is_internal() const -> bool { return !is_leaf(); }
 
 private:
     T m_value{};
-    Node *m_parent{nullptr};
-    std::vector<Node *> m_children{nullptr};
+    Node<T> *m_parent{nullptr};
+    std::vector<Node<T> *> m_children{};
 };
 
 template<typename T>
 class Arvore {
 public:
-    [[nodiscard]] auto size() const -> std::size_t { return 0; }
+    Arvore() = default;
 
-    [[nodiscard]] auto is_empty() const -> bool { return true; }
+    ~Arvore() { delete m_root; }
 
-    [[nodiscard]] auto get_root() const -> Node<T> *;
+    explicit Arvore(const Node<T> &node) {
+        m_root = node;
+    }
 
+    // numero de nós
+    [[nodiscard]] auto size() const -> std::size_t {
+        if (!m_root)
+            return 0;
+        return tree_size(m_root);
+    }
+
+    // se a árvore está vazia
+    [[nodiscard]] auto is_empty() const -> bool {
+        if (m_root)
+            return false;
+        return true;
+    }
+
+    [[nodiscard]] auto get_root() const -> Node<T> * {
+        return m_root;
+    }
+
+    // devolder uma lista com todos os nós
     [[nodiscard]] auto get_nodes() const -> std::vector<Node<T> *>;
 
     // [Algoritmos]
@@ -79,12 +123,45 @@ public:
 
     // [Algoritmos] - Acesso aos nós
 
-    // Mostra o nó console e depois acessa seus filhos
+    // Mostra o nó no console e depois acessa seus filhos
     static auto preorder_print(const Node<T> &node) -> void {
         std::print(*node);
-        for (auto child : node.get_children()) {
+        for (auto child: node.get_children()) {
             std::print(" ");
             preorder_print(child);
+        }
+    }
+
+    // Mostra o nó em preorder, mas com um parêntesis
+    static auto preorder_paren_print(const Node<T> &node) -> void {
+        std::print(*node);
+        if (node.is_internal()) {
+            std::print("( ");
+            for (auto child: node.get_children()) {
+                if (child != child.begin())
+                    std::print(" ");
+                preorder_paren_print(node);
+            }
+            std::print(")");
+        }
+    }
+
+    // Acessa os filhos e mostra o nó
+    static auto postorder_print(const Node<T> &node) -> void {
+        for (auto child: node.get_children()) {
+            postorder_print(child);
+            std::print(" ");
+        }
+        std::print(*node);
+    }
+
+    static auto tree_size(const Node<T> &node) -> std::size_t {
+        std::size_t size{1};
+        if (node.is_internal()) {
+            for (auto child: node.get_children()) {
+                size += tree_size(child);
+            }
+            return size;
         }
     }
 
--- Arvores/main.cpp
+++ Arvores/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include <print>
+#include "Arvore.h"
+
+//  g++ -std=c++23 main.cpp -o main
 
 int main() {
-    std::cout << "Diretório Árvore" << std::endl;
+    auto node = new Node<int>(2);
+    auto node2 = new Node<int>(3);
+
+    node->add_child(*node2);
+    std::println("Node: {}",  **node);
+    for (const auto n : node->get_children())
+    {
+        std::println("{}", **n);
+    }
+
+    delete node;
     return 0;
 }
